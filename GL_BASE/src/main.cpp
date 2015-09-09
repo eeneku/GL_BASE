39dcb3c24a60f3448fde7fefc0040ec1d99f448a
eeneku <eeneku@hotmail.com>
2015-09-09 11:45:09 +0300

    Hinttejä

--- GL_BASE/src/main.cpp
+++ GL_BASE/src/main.cpp
@@ -3,5 +3,12 @@
 
 int main(int argc, char**argv)
 {
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
 	return 0;
 }
